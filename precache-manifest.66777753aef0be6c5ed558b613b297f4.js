94fbaef9d0b08e8c339fa60f3a2efaa8857974c5
henchc <chris.l.hench@gmail.com>
2020-01-18 18:55:59 -0500

    Updates

---
+++ precache-manifest.66777753aef0be6c5ed558b613b297f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6b9af88470c35b5df517faaac634ef",
+    "url": "/MHG-Scansion-WebApp/index.html"
+  },
+  {
+    "revision": "3a8597bc79c51d2fb812",
+    "url": "/MHG-Scansion-WebApp/static/css/main.fec67104.chunk.css"
+  },
+  {
+    "revision": "02df588b606b606b17a5",
+    "url": "/MHG-Scansion-WebApp/static/js/2.2ba2a324.chunk.js"
+  },
+  {
+    "revision": "3a8597bc79c51d2fb812",
+    "url": "/MHG-Scansion-WebApp/static/js/main.051112e2.chunk.js"
+  },
+  {
+    "revision": "5ea4ed2c77d33afc5e20",
+    "url": "/MHG-Scansion-WebApp/static/js/runtime-main.3894216a.js"
+  }
+]);
